fccf2ee065d3c609773fdbde99cdf6380f5c8db9
Bin Ben <bennetthawley@gmail.com>
2018-01-16 13:11:50 -0500

    added more python files during lesson

--- tkinter_test_gui.py
+++ tkinter_test_gui.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
 
-class GUI():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.win.title('Python GUI')
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        master.title('A simple GUI')
+
+        self.label = tk.Label(master, text='This is our first GUI!')
+        self.label.pack()
+
+
+root = tk.Tk()
+my_gui = GUI(root)
+root.mainloop()
