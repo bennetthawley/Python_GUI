019f301ce0161c682a3a8525e18a0b3c3d083c63
Bin Ben <bennetthawley@gmail.com>
2018-02-27 13:50:34 -0500

    Completed style mapper to output all options for each ttk widget element to a .csv file

--- ttk_style_mapper.py
+++ ttk_style_mapper.py
@@ -1,31 +1,43 @@
-import tkinter
+import csv
 from tkinter import ttk
-import pprint
-import json
 
-_widgets = ['TCombobox', 'TEntry', 'TFrame', 'TLabel', 'TLabelFrame','TMenubutton',
-           'TNotebook', 'TPanedwindow', 'Horizontal.TProgressbar', 'Vertical.TProgressbar', 'TRadiobutton',
-           'Horizontal.TScale', 'Vertical.TScale', 'Horizontal.TScrollbar', 'Vertical.Scrollbar', 'TSeparator',
-           'TSizegrip', 'Treeview']
-
-widgets = {'TButton':['Button.padding','Button.label'],
-           'TCheckbutton':['Checkbutton.padding', 'Checkbutton.label']}
+widgets = {'TButton': ['Button.padding', 'Button.label'],
+           'TCheckbutton': ['Checkbutton.padding', 'Checkbutton.label'],
+           'TCombobox': ['Combobox.padding', 'Combobox.textarea'],
+           'TEntry': ['Entry.padding', 'Entry.textarea'],
+           'TLabel': ['Label.padding', 'Label.label'],
+           'TMenubutton': ['Menubutton.padding', 'Menubutton.label'],
+           'TRadiobutton': ['Radiobutton.padding', 'Radiobutton.label'],
+           'Horizontal.TScale': ['Horizontal.Scale.trough'],
+           'Vertical.TScale': ['Vertical.Scale.trough'],
+           'Horizontal.TScrollbar': ['Horizontal.Scrollbar.trough', 'Horizontal.Scrollbar.leftarrow',
+                                     'Horizontal.Scrollbar.rightarrow', 'Horizontal.Scrollbar.thumb'],
+           'Vertical.TScrollbar': ['Vertical.Scrollbar.trough', 'Vertical.Scrollbar.uparrow',
+                                   'Vertical.Scrollbar.downarrow', 'Horizontal.Scrollbar.thumb'],
+           'Treeview': ['Treeview.field', 'Treeview.padding']}
 
 style = ttk.Style()
 
+
 def print_elements(widget):
     layout = style.layout(widget)
-    pprint.pprint(layout)
+    return layout
+
 
 def print_options(element):
     options = style.element_options(element)
-    pprint.pprint(options)
-
-for key in widgets.keys():
-    print(key)
-    for value in widgets[key]:
-        print(value)
-        print_options(value)
+    return options
 
 
+def write_csv(name):
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Widget', 'Element', 'Options']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for key in widgets.keys():
+            for value in widgets[key]:
+                element_options = print_options(value)
+                writer.writerow({fieldnames[0]: key, fieldnames[1]: value, fieldnames[2]: element_options})
 
+
+write_csv('ttk_style_options.csv')
